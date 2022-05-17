a4447908d43b37bf19f988027cf1f1a43cda364d
Psalm Tobias <psalm@example.com>
2022-05-18 02:46:30 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
+import styles from './css/style.module.css'
+import Navbar from './components/Navbar'
+import { UserProvider } from './UserContext'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// import io from 'socket.io-client'
+// const socket = io.connect("http://localhost:4000");
+// client-side
+// socket.on("connect", () => {
+//   console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+// });
+
+
+export default function App() {
+   const [user, setUser] = useState({
+        id: null
+    })
+
+    useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users/details`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data._id){
+                setUser({
+                    id: data._id,
+                    accessType: data.accessType
+
+                })
+            }else{
+                setUser({
+                    id: null
+                })
+            }            
+        })
+    }, [user.id])
+
+    const unsetUser = () => {
+        localStorage.clear()
+
+        setUser({
+            id: null
+        });
+    }
+
+   return (
+      <UserProvider value={{user, setUser, unsetUser}}>
+         <Navbar />
+      </UserProvider>
+   );
 }
 
-export default App;
+
+
